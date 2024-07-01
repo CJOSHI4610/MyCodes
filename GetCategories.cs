bafbff35ab84009f2f213a0bed7a9ac63527bebc
CJOSHI4610 <c.joshi4610@gmail.com>
2024-07-01 20:36:50 +0530

    Commit message

--- GetCategories.cs
+++ GetCategories.cs
@@ -1,13 +1,13 @@
+using System;
+
 namespace MyConsoleApp
 {
-    // This is a simple console application that prints "Hello, World!"
     class Program
     {
         static void Main(string[] args)
         {
-            Consol.WriteLine("Hello, World!");
-            // Introduce a syntax error by removing a semicolon
-            Consoool.WriteLine("This line is missing a semicolon")
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This line is missing a semicolon");
         }
     }
 }
