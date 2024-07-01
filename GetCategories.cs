0a97c586aecc9f171a2aba9a2004ff1a1bd5dfc7
CJOSHI4610 <c.joshi4610@gmail.com>
2024-07-01 17:06:10 +0530

    Commit message

--- GetCategories.cs
+++ GetCategories.cs
@@ -1,7 +1,12 @@
-public void PrintSubCategories(List subCategories,,,)
+public void PrintSubCategories(List subCategories, int level)
 {
-    var output = new St
+    var output = new StringBuilder();
+
+    foreach (var subCategory in subCategories)
+    {
+        output.Append(new string(' ', level * 2) + $"SubCategory: {subCategory.Name}");
+        PrintSubCategories(subCategory.SubCategories, level + 2);
     }
 
-    Console.WriteLine(output.ToString());
+    Console.WriteLine(output.ToStr
 }
