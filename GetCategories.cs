8e1d0940b4cde3a9e5a765ebc165710c5fba1184
CJOSHI4610 <c.joshi4610@gmail.com>
2024-07-04 12:16:18 +0530

    Commit message

--- GetCategories.cs
+++ GetCategories.cs
@@ -1,13 +1,5 @@
-using System;
-
-namespace MyConsoleApp
-{
-    class Program
-    {
-        static void Main(string[] args)
+ static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("This line is missing a semicolon");
+            Console.WriteLine("jai mahishmati");
         }
-    }
-}
